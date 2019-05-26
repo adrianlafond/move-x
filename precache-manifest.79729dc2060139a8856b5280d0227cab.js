6211ae2a3c6ae88725f155ba2d31ceab085ee9d3
Adrian Lafond <mail@adrianlafond.com>
2019-05-26 16:51:07 -0400

    Updates

---
+++ precache-manifest.79729dc2060139a8856b5280d0227cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1b0088efef23db7c0f23a9770f0b29",
+    "url": "/move/index.html"
+  },
+  {
+    "revision": "b1190ca44782b703be99",
+    "url": "/move/static/css/main.3a39e7d6.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/move/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "b1190ca44782b703be99",
+    "url": "/move/static/js/main.759fc774.chunk.js"
+  },
+  {
+    "revision": "45e1a6409270d579217b",
+    "url": "/move/static/js/runtime~main.9e2cdc44.js"
+  }
+]);
